13da194c785c7c64609d5394b475740906b34d63
Abner <sbner2@gmail.com>
2025-04-23 05:46:31 -0300

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProfileComponent } from './components/profile/profile.component';
-
-export const routes: Routes = [
-    {path: 'profile', component: ProfileComponent},
-];
--- src/app/components/thank-you/thank-you.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-thank-you',
-  standalone: true,
-  templateUrl: './thank-you.component.html',
-  styleUrl: './thank-you.component.scss'
-})
-export class ThankYouComponent {}
